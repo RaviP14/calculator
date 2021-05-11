e1e80370ba8b955f01917e08658ca27fc684f228
RaviP14 <ravi.patel14@outlook.com>
2021-05-11 19:18:37 +0100

    added buttons, display and input using js.

--- script.js
+++ script.js
@@ -2,26 +2,56 @@ function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num3, num4) {
-    return num3 - num4;
+function subtract(num1, num2) {
+    return num1 - num2;
 }
 
-function multiply(num5, num6) {
-    return num5 * num6;
+function multiply(num1, num2) {
+    return num1 * num2;
 }
 
-function divide(num7, num8) {
-    return num7 / num8;
+function divide(num1, num2) {
+    return num1 / num2;
 }
 
-function operate(operator, num9, num10) {
+function operate(operator, num1, num2) {
     if (operator === 'add') {
-        add(num9, num10);
+        return add(num1, num2);
     }else if (operator === 'subtract') {
-        subtract(num9, num10);
+        return subtract(num1, num2);
     } else if (operator === 'multiply') {
-        multiply(num9, num10);
+       return multiply(num1, num2);
     } else if (operator === 'divide') {
-       divide(num9, num10);
+       return divide(num1, num2);
     }
 }
+
+function data(valueA, symbol) {
+   const displays = document.querySelector('.display');
+   //let values = displays.textContent = valueA;
+   let operation = displays.textContent = valueA + " " + symbol;
+   console.log(operation)
+   if (valueA === true) { //problem with if statment also values and operation get shown straight away
+        return values
+   } else if ((valueA === true) && (symbol === true )) {
+       return operation
+   }
+   
+}
+const inputNum = document.querySelectorAll('.numberBtn');
+
+inputNum.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const valueA = e.target.value
+        data(valueA, '')
+    })
+})
+
+ const inputOp = document.querySelectorAll('.operatorBtn');
+
+ inputOp.forEach((button) => {
+     button.addEventListener('click', (e) => {
+        const symbol = e.target.value
+        data('', symbol)
+     })
+ })
