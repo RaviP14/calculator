26bd1f42fa8c2d1ee20a69235038606c8f44d8e3
RaviP14 <ravi.patel14@outlook.com>
2021-05-13 14:16:57 +0100

    calculator works properly now & decimal added.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function add(num1, num2) {
-    return parseInt(num1) + parseInt(num2);
+    totals = parseFloat(num1) + parseFloat(num2);
+    return rounded = Math.round(totals * 1000)/1000
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    totals = num1 - num2;
+    return rounded = Math.round(totals * 1000)/1000
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    totals =  num1 * num2;
+    return rounded = Math.round(totals * 1000)/1000
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    totals = num1 / num2;
+    return rounded = Math.round(totals * 1000)/1000
 }
 
 function operate(operator, num1, num2) {
@@ -25,25 +29,35 @@ function operate(operator, num1, num2) {
        return divide(num1, num2);
     }
 }
-
+//when equals is pressed
 function calc() {
     total = operate(symbol, value1, value2);
     const displays = document.querySelector('.display');
-    displays.textContent = total
+    if (total !== Infinity) {
+        displays.textContent = total
+    } else if (total === Infinity) {
+        displays.textContent = 'Really?'
+    }
     console.log(total)
     symbol = undefined
     value1 = total
     value2 = ''
-    
+    decimalVal = undefined
+    decimalVal2 = undefined
 }
-
+//for continuous calcs
 function calc2() {
-    total = operate(symbol, value1, value2);
     const displays = document.querySelector('.display');
-    displays.textContent = total;
+    if (total !== Infinity) {
+        displays.textContent = total
+    } else if (total === Infinity) {
+        displays.textContent = 'Really?'
+    }
     console.log(total)
     value1 = total
     value2 = ''
+    decimalVal = undefined
+    decimalVal2 = undefined
 }
 //transfer symbol, value1 & value2 into operate function
 let equals = document.querySelector('.equals');
@@ -51,7 +65,10 @@ let equals = document.querySelector('.equals');
 let total;
 
 equals.addEventListener('click', () => {
-    calc()
+    if (value1 !== '' && value2 !== '' && symbol !== undefined) {
+        calc()
+    }
+    
 })
 // get value1 & value2
 let inputNum = document.querySelectorAll('.numberBtn');
@@ -73,8 +90,31 @@ inputNum.forEach((button) => {
             displays.textContent = '';
             displays.textContent = value2
             console.log(value2)
+            total = operate(symbol, value1, value2);
         }
     })
+})
+
+//decimal point - not working with + (ignores decimals) 
+
+let decimal = document.querySelector('.decimal');
+
+let decimalVal;
+let decimalVal2;
+
+decimal.addEventListener('click', (e) => {
+    const displays = document.querySelector('.display');
+    if ((displays.textContent === value1) && decimalVal === undefined) {
+        decimalVal = e.target.value
+        value1 += decimalVal
+        console.log(value1)
+        displays.textContent = value1
+    } else if ((displays.textContent === value2) && decimalVal2 == undefined) {
+        decimalVal2 = e.target.value
+        value2 += decimalVal2
+        console.log(value2)
+        displays.textContent = value2
+    }
 })
 
 //get operator
@@ -110,6 +150,8 @@ cleared.addEventListener('click', () => {
     symbol = undefined
     value1 = ''
     value2 = ''
+    decimalVal = undefined
+    decimalVal2 = undefined
     const displays = document.querySelector('.display');
     displays.textContent = '';
 })
